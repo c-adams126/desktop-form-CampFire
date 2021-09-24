a719a1066cd20d364ae286f0010d6745f6be8be8
kalgo101 <44305744+kalgo101@users.noreply.github.com>
2021-09-24 13:01:31 -0400

    user model

---
+++ WinFormsApp1/WinFormsApp1/user.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class user
+    {
+        public string UserName { get; set; }
+
+        
+        public string PassWord { get; set; }
+    }
+}
